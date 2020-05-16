e74108a107d6f7cf5b7b4105d0850e97a12f8ea5
yuvraj <ymakode@falabella.cl>
2020-05-16 18:43:46 +0530

    newman run collection using script

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## Install Newman globally on your system using Homebrew.
+
+`brew install newman`
+
+## Install packege using below command
+
+`npm install newman express fs body-parser`
+
+## Run command to execute collection
+`node script.js`
---
+++ script.js
@@ -0,0 +1,25 @@
+var fs = require('fs'),
+    newman = require('newman'),
+
+    results = []; // require newman in your project
+ 
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('./sample-collection.json'),
+    environment: require('./environment.json'),
+    iterationData: '/Users/yuvraj/Downloads/newman/data_postman.csv',
+    reporters: 'cli'
+}).on('request', function (err, args) {
+    if (!err) {
+        // here, args.response represents the entire response object
+        var rawBody = args.response.stream, // this is a buffer
+            body = rawBody.toString(); // stringified JSON
+
+        results.push(JSON.parse(body)); // this is just to aggregate all responses into one object
+    }
+})
+// a second argument is also passed to this handler, if more details are needed.
+.on('done', function (err, summary) {
+    // write the details to any file of your choice. The format may vary depending on your use case
+    fs.writeFileSync('migration-report.json', JSON.stringify(results, null, 4));
+});
